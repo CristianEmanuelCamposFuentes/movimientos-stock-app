22d781345a1a06866bcee7abfdd3680cf2a0136d
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-09-02 21:30:03 -0300

    add relative paths

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from modules.form import create_form
+import os
+from sqlalchemy import create_engine
+
+
+
+# Obtener la ruta absoluta del directorio donde está main.py
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construir la ruta de la base de datos relativa al directorio de main.py
+db_path = os.path.join(base_dir, 'data', 'stock_management.db')
+
+# Crear el motor de SQLAlchemy con la ruta relativa
+engine = create_engine(f'sqlite:///{db_path}')
+
+print(f"Ruta de la base de datos: {db_path}")
 
 if __name__ == "__main__":
     create_form()
-
