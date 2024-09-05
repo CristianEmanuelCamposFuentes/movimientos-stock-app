f852e5f8444a58979706e7cd90588f640620a314
CristianEmanuelCamposFuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-09-05 07:37:39 -0300

    add relative routes

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from modules.form import create_form
+import sys
+from PyQt5.QtWidgets import QApplication
+from modules.form import StockMovementApp
+from modules.main_window import MainWindow   # Asegúrate de que la clase esté en el archivo correcto
 import os
 from sqlalchemy import create_engine
 
@@ -14,4 +17,20 @@ engine = create_engine(f'sqlite:///{db_path}')
 print(f"Ruta de la base de datos: {db_path}")
 
 if __name__ == "__main__":
-    create_form()
+    app = QApplication(sys.argv)
+    ventana = MainWindow()  # Inicializar la aplicación de PyQt
+    #ventana = StockMovementApp()    Crear una instancia de la interfaz
+    ventana.show()                 # Mostrar la ventana
+    sys.exit(app.exec_())          # Ejecutar el bucle principal de la aplicación
+
+app.setStyleSheet("""
+QWidget {
+    background-color: #F0F0F0;
+}
+QPushButton {
+    background-color: #4CAF50;
+    color: white;
+    font-size: 16px;
+    border-radius: 5px;
+}
+""")
