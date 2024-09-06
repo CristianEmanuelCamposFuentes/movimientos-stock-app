f361fe1917fed5352754e38fb8a64ef5a59a9571
CristianEmanuelCamposFuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-09-06 14:18:46 -0300

    create login.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
 from PyQt5.QtWidgets import QApplication
-from modules.main_window import MainWindow   # Asegúrate de que la clase esté en el archivo correcto
+from modules.login import LoginWindow  # Importa la ventana de Login
 import os
 from sqlalchemy import create_engine
 
@@ -15,15 +14,8 @@ engine = create_engine(f'sqlite:///{db_path}')
 
 print(f"Ruta de la base de datos: {db_path}")
 
-# if __name__ == "__main__":
-#     app = QApplication(sys.argv)
-#     ventana = MainWindow()  
-#     #ventana = StockMovementApp()   
-#     ventana.show()                 
-#     sys.exit(app.exec_())          
-
 if __name__ == "__main__":
-    app = QApplication([]) # Inicializar la aplicación de PyQt
-    ventana = MainWindow() # Crear una instancia de la interfaz
-    ventana.show() # Mostrar la ventana
-    app.exec_() # Ejecutar el bucle principal de la aplicación
+    app = QApplication([])  # Inicializar la aplicación de PyQt
+    ventana_login = LoginWindow()  # Crear una instancia de la ventana de login
+    ventana_login.show()  # Mostrar la ventana de login
+    app.exec_()  # Ejecutar el bucle principal de la aplicación
