0da7ecd9d822661e7b8f3dcc5ba20f7eb6808941
CristianEmanuelCamposFuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-09-09 16:00:30 -0300

    change old files

--- modules/pedidos.py
+++
@@ -1,7 +0,0 @@
-from PyQt5.QtWidgets import QMessageBox
-
-def abrir_nota_pedido():
-    msg = QMessageBox()
-    msg.setWindowTitle("Generar Nota de Pedido")
-    msg.setText("Se abrirá la ventana para generar una nota de pedido.")
-    msg.exec_()
