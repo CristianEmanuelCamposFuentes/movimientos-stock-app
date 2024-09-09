a7862896681b61989b2dfcb53f625ecfb75b6579
CristianEmanuelCamposFuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-09-09 16:00:56 -0300

    add styles

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from PyQt5.QtWidgets import QApplication
 from modules.login import LoginWindow  # Importa la ventana de Login
 import os
 from sqlalchemy import create_engine
+from modules.ui_styles import aplicar_estilo_global  # Importa la función de estilos globales
+
 
 # Obtener la ruta absoluta del directorio donde está main.py
 base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -17,6 +19,7 @@ print(f"Ruta de la base de datos: {db_path}")
 
 if __name__ == "__main__":
     app = QApplication([])  # Inicializar la aplicación de PyQt
+    aplicar_estilo_global(app)  # Aplicar los estilos globales
     ventana_login = LoginWindow()  # Crear una instancia de la ventana de login
     ventana_login.show()  # Mostrar la ventana de login
     app.exec_()  # Ejecutar el bucle principal de la aplicación
